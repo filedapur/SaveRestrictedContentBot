27db0e42c03a6d44231fff1466cae6d97389e768
filedapur <125831535+filedapur@users.noreply.github.com>
2023-02-20 04:41:43 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
+API_ID = 28849983
+API_HASH = "f57ad31a42a8994f5789b2ad162e3cd4"
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
+SESSION = "1AZWarzYBu46OLTzFMoQp-LGWrjh36BD4hVYpQmIkWQIw0osKNA8_o2R-0UpXD6H6NBmApqV0S3mtzIrEDs3Wg5Rezce3d5qJ5RoWzEDIUYGvxHEep5SOL6vu5D0E-QU4czuAHirBFBvWY1alcK4nIkPmXysGtxVVrD8RUklYbRpiBeFv89BGC-pfF9Ek0C2XHffgrMLU9RZ5fS4ZSH0pgHVD5ZjWAz9LIiDYWv1eDId-t3jZl9WueuiiLDfNJ7i5_HgI2dHwyxqRYaAtEp8TbBmiHqDRw0t-oHNAd3LMk3yIQus4UHLtJcLEArVEY_Cm6aoWkAIbMaM0uXal5jD0VJDz31TwInY="
 FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+AUTH = 1812404420
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
